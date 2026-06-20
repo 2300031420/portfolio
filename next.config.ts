5927bb67cc78105d12795f4aea1d5ea17ab3e139
Saneesh Reddy <sanukyj@gmail.com>
2026-06-20 11:35:31 +0530

    small changes

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  basePath: "/portfolio",
 };
 
 export default nextConfig;
